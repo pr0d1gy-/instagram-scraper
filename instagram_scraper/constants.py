2b325b7ee1b76a0b96047c111af67e4a8830c768
Kirill Portenko <portenkok@gmail.com>
2018-04-23 16:19:49 +0300

    change first to 50 instead of 100 for location and media vars

--- instagram_scraper/constants.py
+++ instagram_scraper/constants.py
@@ -21,10 +21,10 @@ QUERY_HASHTAG = BASE_URL + 'graphql/query/?query_hash=ded47faa9a1aaded10161a2ff3
 QUERY_HASHTAG_VARS = '{{"tag_name":"{0}","first":100,"after":"{1}"}}'
 
 QUERY_LOCATION = BASE_URL + 'graphql/query/?query_hash=ac38b90f0f3981c42092016a37c59bf7&variables={0}'
-QUERY_LOCATION_VARS = '{{"id":"{0}","first":100,"after":"{1}"}}'
+QUERY_LOCATION_VARS = '{{"id":"{0}","first":50,"after":"{1}"}}'
 
 QUERY_MEDIA = BASE_URL + 'graphql/query/?query_hash=42323d64886122307be10013ad2dcc44&variables={0}'
-QUERY_MEDIA_VARS = '{{"id":"{0}","first":100,"after":"{1}"}}'
+QUERY_MEDIA_VARS = '{{"id":"{0}","first":50,"after":"{1}"}}'
 
 MAX_CONCURRENT_DOWNLOADS = 5
 CONNECT_TIMEOUT = 90
